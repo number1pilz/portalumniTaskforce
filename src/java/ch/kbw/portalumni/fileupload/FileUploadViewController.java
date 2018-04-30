045d5ba00fd051dfbdd85d2f7cda28016498b3af
Silvan Baach <silvan.baach@gmail.com>
2018-04-30 13:34:35 +0200

    Merge

--- src/java/ch/kbw/portalumni/fileupload/FileUploadViewController.java
+++ src/java/ch/kbw/portalumni/fileupload/FileUploadViewController.java
@@ -5,17 +5,36 @@
  */
 package ch.kbw.portalumni.fileupload;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.model.UploadedFile;
 
 /**
  *
  * @author Silvan Baach
  *
- * 
+ *
  */
 @ManagedBean
 @SessionScoped
 public class FileUploadViewController {
-    
+
+    private UploadedFile file;
+
+    public UploadedFile getFile() {
+        return file;
+    }
+
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+
+    public void upload() {
+        if (file != null) {
+            FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
 }
